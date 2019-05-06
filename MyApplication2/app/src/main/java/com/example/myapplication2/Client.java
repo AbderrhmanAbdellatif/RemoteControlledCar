d998f1de7d06dafc28cf288bc85b20c385f0ce3f
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-05-06 17:44:33 +0300

    android

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/Client.java
@@ -0,0 +1,63 @@
+package com.example.myapplication2;
+
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author INSECT
+ */
+public class Client {
+
+    //her clientın bir soketi olmalı
+    public static Socket socket;
+
+    //verileri almak için gerekli nesne
+    public static ObjectInputStream sInput;
+    //verileri göndermek için gerekli nesne
+    public static ObjectOutputStream sOutput;
+
+    public static void Start(String ip, int port,String message) {
+        MainActivity m = new MainActivity();
+        MainActivity.text.setText("Connected to server");
+        try {
+            // Client Soket nesnesi
+
+            if( (Client.socket = new Socket(ip, port))!=null ){
+                Client.Display("Connected to server");
+                Client.sInput = new ObjectInputStream(Client.socket.getInputStream());
+                // output stream
+                Client.sOutput = new ObjectOutputStream(Client.socket.getOutputStream());
+                Client.Display(Client.sInput.readObject().toString());
+                Client.sOutput.writeObject(message);
+
+                sOutput.flush();
+                sOutput.close();
+                socket.close();
+
+            }else{
+                MainActivity.text.setText("Unable to connect to server");
+            }
+            //Client.socket = new Socket(ip, port);
+
+
+        } catch (IOException ex) {
+            //Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            Client.Display("Unable to connect to server");
+        } catch (ClassNotFoundException ex) {
+            //Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            Client.Display("Unable to connect to server");
+        }
+    }
+
+    public static void Display(String msg) {
+        System.out.println(msg);
+    }
+
+}
---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.myapplication2;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.burak_000.client.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button forward;
+    Button back;
+    Button sag;
+    Button sol;
+    Button dur;
+    Button conn;
+    EditText ipT;
+    EditText portT;
+
+
+    public static TextView text;
+
+    private Socket s;
+    private ServerSocket ss;
+    private static InputStreamReader isr;
+    private static BufferedReader br;
+    private static PrintWriter pr;
+
+    String message="";
+    static String ip="";
+    static int port;
+    final Context context = this;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        forward = (Button) findViewById(R.id.ileri);
+        back = (Button) findViewById(R.id.geri);
+        sag = (Button) findViewById(R.id.sag);
+        sol = (Button) findViewById(R.id.sol);
+        dur = (Button) findViewById(R.id.dur);
+        conn = (Button) findViewById(R.id.conn);
+        text = (TextView)findViewById(R.id.textView);
+        ipT = (EditText) findViewById(R.id.ipText);
+        portT = (EditText) findViewById(R.id.portText);
+
+        MyTask mt = new MyTask();
+        mt.execute();
+
+    }
+
+    class MyTask extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... params){
+
+            conn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    message = "b";
+                    ip=ipT.getText().toString();
+                    port=Integer.parseInt(portT.getText().toString());
+                    try{
+                        Client.Start(ip, port,message);
+                        text.setText("Connected to server");
+                    }catch (ExceptionInInitializerError ex){
+                        text.setText("Unable to connect to server");
+                    }
+
+                }
+            });
+
+            forward.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+
+                    message = "İleri";
+                    Client.Start(ip, port,message);
+
+                    /*AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                            context);
+                    //message = messageBox.getText().toString();
+                    Client.Start(ip, port,message);
+                    alertDialogBuilder.setTitle(message);
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();*/
+
+                }
+            });
+
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    //text.setText();
+                    message = "Geri";
+                    Client.Start(ip, port,message);
+                }
+            });
+
+            sag.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    //text.setText("Geri gidiyor");
+                    message = "Sag";
+                    Client.Start(ip, port,message);
+                }
+            });
+
+            sol.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    //text.setText("Geri gidiyor");
+                    message = "Sol";
+                    Client.Start(ip, port,message);
+                }
+            });
+
+            dur.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    //text.setText("Geri gidiyor");
+                    message = "Dur";
+                    Client.Start(ip, port,message);
+                }
+            });
+
+            /*sol360.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    //text.setText("Geri gidiyor");
+                    message = "360";
+                    Client.Start(ip, port,message);
+                }
+            });*/
+
+
+            return null;
+        }
+
+    }
+}
