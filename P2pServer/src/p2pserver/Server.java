afb7798ce84a91b14579da07c8536a30b9e3fad5
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-05-07 17:16:09 +0100

    Update Server.java

--- P2pServer/src/p2pserver/Server.java
+++ P2pServer/src/p2pserver/Server.java
@@ -1,10 +1,10 @@
 package p2pserver;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -19,109 +19,104 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author INSECT
- */
-
-public class Server{
+public class Server {
 
     //server soketi eklemeliyiz
     public static ServerSocket serverSocket;
     // Serverın dileyeceği port
     public static int port = 0;
+  /*  GpioPinDigitalOutput out0 =null ;
+    GpioPinDigitalOutput out2 =null ;
+    GpioPinDigitalOutput out3 =null ;
+    GpioPinDigitalOutput out4 = null ;*/
 
     public static void Start(int openport) {
-		final GpioController gpio = GpioFactory.getInstance(); //pinleri tanımlama 
-		final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
-        	final GpioPinDigitalOutput out2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
-		final GpioPinDigitalOutput out3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);
-        	final GpioPinDigitalOutput out4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
+        final GpioController gpio = GpioFactory.getInstance(); //pinleri tanımlama 
+        final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
+        final GpioPinDigitalOutput out2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
+        final GpioPinDigitalOutput out3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);
+        final GpioPinDigitalOutput out4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
+//
+//    final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_19, PinState.LOW);
+//    final GpioPinDigitalOutput out2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, PinState.LOW);
+//    final GpioPinDigitalOutput out3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_21, PinState.LOW);
+//    final GpioPinDigitalOutput out4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_20, PinState.LOW);
 
-            try {
-                Server.port = openport;
-                // serversoket nesnesi
-                Server.serverSocket = new ServerSocket(Server.port);
-                
-                while (true) {
-                Server.Display("Client Bekleniyor!!");
+        try {
+            Server.port = openport;
+            // serversoket nesnesi
+            Server.serverSocket = new ServerSocket(Server.port);
+            Server.Display("Client Bekleniyor...");
+
+            while (true) {
 
                 Socket clientSocket = Server.serverSocket.accept();
 
-                Server.Display("Client Geldi...(:");
+                Server.Display("Client Geldi...");
 
                 ObjectOutputStream sOutput = new ObjectOutputStream(clientSocket.getOutputStream());
                 ObjectInputStream sInput = new ObjectInputStream(clientSocket.getInputStream());
 
-                sOutput.writeObject("Server:welcome");
-                
-                String message = sInput.readObject().toString(); 
+                sOutput.writeObject("Server:Hosgeldin");
+
+                String message = sInput.readObject().toString();
                 //Server.Display();
-                
-                if(message!=null && message.equals("İleri")){
-		Server.Display("ileri");
-	
+
+                if (message != null && message.equals("İleri")) {
+                    Server.Display("ileri");
+
                     out0.high();
-		    out4.high();
-		    out2.low();
-    	            out3.low();
-			}
-                else if(message!=null && message.equals("Geri")){
-					Server.Display("Geri");
+                    out4.high();
 
+                    out2.low();
+                    out3.low();
+
+                } else if (message != null && message.equals("Geri")) {
+                    Server.Display("Geri");
                     out0.low();
-		    out4.low();
-		    out2.high();
-    	            out3.high();
-                }
-		else if(message!=null && message.equals("Sag")){
-								Server.Display("sag");
-
+                    out4.low();
+                    out2.high();
+                    out3.high();
+                } else if (message != null && message.equals("Sag")) {
+                    Server.Display("Sag");
                     out0.low();
-		    out2.low();
-    	            out3.low();
-		    out4.high();
-		    Thread.sleep(600);
-		    out4.low();
-		    
-                }
-		else if(message!=null && message.equals("Sol")){
-											Server.Display("Sol");
+                    out2.low();
+                    out3.low();
+                    out4.high();
+                    Thread.sleep(500);
+                    out4.low();
 
-		    out4.low();
-		    out2.low();
-    	            out3.low();
-	 	    out0.high();
-		    Thread.sleep(600);
-		    out0.low();
-                }
-		else if(message!=null && message.equals("360")){
-		    out4.low();
-		    out2.low();
-    	            out3.low();
-	 	    out0.high();
-		    Thread.sleep(1600);
-		    out0.low();
-                }
-		else if(message!=null && message.equals("Dur")){
-														Server.Display("Dur");
+                } else if (message != null && message.equals("Sol")) {
+                    Server.Display("Sol");
+                    out4.low();
+                    out2.low();
+                    out3.low();
+                    out0.high();
+                    Thread.sleep(500);
+                    out0.low();
+                } else if (message != null && message.equals("360")) {
+                    out4.low();
+                    out2.low();
+                    out3.low();
+                    out0.high();
+                    Thread.sleep(1750);
+                    out0.low();
+                } else if (message != null && message.equals("Dur")) {
+                    out4.low();
+                    out2.low();
+                    out3.low();
+                    out0.low();
 
-		    out4.low();
-		    out2.low();
-    	     	    out3.low();
-	 	    out0.low();
-		   
                 }
             }
 
-            } catch (IOException ex) {
-                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            catch (Exception e) {
-                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, e);
-            }
+        } catch (IOException ex) {
+            Logger.getLogger(Testserver.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Testserver.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            Logger.getLogger(Testserver.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 
     public static void Display(String msg) {
