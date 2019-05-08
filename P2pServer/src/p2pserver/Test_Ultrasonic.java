cc13f8465a23ad8b82cc38640ad8369932224e05
Şahin Alçın <36476466+sahinalcin@users.noreply.github.com>
2019-05-08 22:57:34 +0300

    Update Test_Ultrasonic.java

--- P2pServer/src/p2pserver/Test_Ultrasonic.java
+++ P2pServer/src/p2pserver/Test_Ultrasonic.java
@@ -12,7 +12,7 @@ public class Test_Ultrasonic {
 
     public static void main(String[] args) throws Exception {
         final GpioController gpio = GpioFactory.getInstance(); //pinleri tanımlamasi 
-        final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW); //pin 00 tanimladim
+        final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);//pin 00 tanimladim
         final GpioPinDigitalOutput out2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);//pin 02 tanimladim
         final GpioPinDigitalOutput out3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);//pin 03 tanimladim
         final GpioPinDigitalOutput out4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);//pin 04 tanimladim
@@ -29,11 +29,11 @@ public class Test_Ultrasonic {
             System.out.println("distance " + sonic.getDistance() / 10 + "cm");
             Thread.sleep(100); //1s
             
-            if ((sonic.getDistance() / 10) < 30) {// yakin oldugu zaman mesafa hesaplama yani 30 cmden daha az ise
-                out4.low(); // pin 04 durdurmak
-                out2.low(); // pin 02 durdurmak
-                out3.low(); // pin 03 durdurmak 
-                out0.low();  // pin 00 durdurmak
+            if ((sonic.getDistance() / 10) < 30) {// 30 cmden daha az mesafede sensorun arabayi durdurmasi icin
+                out4.low(); // pin 04'u durdurur
+                out2.low(); // pin 02'u durdurur
+                out3.low(); // pin 03'u durdurur 
+                out0.low(); // pin 00'u durdurur
             }
         }
     }
