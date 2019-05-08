8f19a91475b195a0491bd14a091de3c979c276e7
atahanata <32178476+atahanata@users.noreply.github.com>
2019-05-08 13:13:58 +0300

    Update Test_Ultrasonic.java

--- P2pServer/src/p2pserver/Test_Ultrasonic.java
+++ P2pServer/src/p2pserver/Test_Ultrasonic.java
@@ -12,10 +12,10 @@ public class Test_Ultrasonic {
 
     public static void main(String[] args) throws Exception {
         final GpioController gpio = GpioFactory.getInstance(); //pinleri tanımlamasi 
-        final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
-        final GpioPinDigitalOutput out2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
-        final GpioPinDigitalOutput out3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);
-        final GpioPinDigitalOutput out4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
+        final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW); //pin 00 tanimladim
+        final GpioPinDigitalOutput out2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);//pin 02 tanimladim
+        final GpioPinDigitalOutput out3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);//pin 03 tanimladim
+        final GpioPinDigitalOutput out4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);//pin 04 tanimladim
 //
         PiJavaUltrasonic sonic = new PiJavaUltrasonic(
                 28,//ECO 11
@@ -29,11 +29,11 @@ public class Test_Ultrasonic {
             System.out.println("distance " + sonic.getDistance() / 10 + "cm");
             Thread.sleep(1000); //1s
             
-            if ((sonic.getDistance() / 10) < 30) {
-                out4.low();
-                out2.low();
-                out3.low();
-                out0.low();
+            if ((sonic.getDistance() / 10) < 30) {// yakin oldugu zaman mesafa hesaplama yani 30 cmden daha az ise
+                out4.low(); // pin 04 durdurmak
+                out2.low(); // pin 02 durdurmak
+                out3.low(); // pin 03 durdurmak 
+                out0.low();  // pin 00 durdurmak
             }
         }
     }
