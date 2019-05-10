7d1381d38f41985d49d0eb0a19a23904cf7b231f
yrgp <43738942+yrgp@users.noreply.github.com>
2019-05-10 12:04:41 +0100

    Update Server.java

--- P2pServer/src/p2pserver/Server.java
+++ P2pServer/src/p2pserver/Server.java
@@ -16,28 +16,32 @@ import java.util.logging.Logger;
 public class Server {
 
     //server soketinin eklenmesi
-    public static ServerSocket serverSocket;
     // Serverin dileyeceği port
     public static int port = 0;
+    public static int ileriGeri = 2;
 
-    public static void Start(int openport) {
-       //Kullanilacak 4 pinin tanimlanmasi
+    public static ServerSocket serverSocket;
+    public static Socket clientSocket;
+
+    public static void Start(int serverPort) {
+
+        //Kullanilacak 4 pinin tanimlanmasi
         final GpioController gpio = GpioFactory.getInstance();
         final GpioPinDigitalOutput out0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
         final GpioPinDigitalOutput out2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
         final GpioPinDigitalOutput out3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);
         final GpioPinDigitalOutput out4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
 
-
         try {
-            Server.port = openport;
+            Server.port = serverPort;
 
-            Server.serverSocket = new ServerSocket(Server.port);
+            Server.serverSocket = new ServerSocket(Server.port); 
+            
             Server.Display("Client Bekleniyor...");
 
             while (true) {
 
-                Socket clientSocket = Server.serverSocket.accept();
+                clientSocket = Server.serverSocket.accept();
 
                 Server.Display("Client Geldi...");
 
@@ -46,16 +50,17 @@ public class Server {
 
                 sOutput.writeObject("Server:Merhabalar");
 
-                String message = sInput.readObject().toString();
+                String message = sInput.readObject().toString(); 
                 //Server.Display();
-
                 if (message != null && message.equals("İleri")) {
                     Server.Display("ileri");
 
-                    out0.high(); //ileri sol tekerlik aktif olacak
-                    out4.high(); //ileri sag tekerlik aktif olacak
-                    out2.low(); 
+                    out0.high();
+                    out4.high();
+                    out2.low();
                     out3.low();
+
+                    ileriGeri = 0;
 
                 } else if (message != null && message.equals("Geri")) {
                     Server.Display("Geri");
@@ -63,53 +68,80 @@ public class Server {
                     out4.low();
                     out2.high();
                     out3.high();
-                    
+                    ileriGeri = 1;
                 } else if (message != null && message.equals("Sag")) {
                     Server.Display("Sag");
-                    out0.low();
+
+                    out4.low();
                     out2.low();
+                    out3.high();
+                    out0.high();
+                    Thread.sleep(500);
+                    out3.low();
+                    out0.low();
+                    if (ileriGeri == 0) {
+
+                        out0.high();
+                        out4.high();
+                        out2.low();
+                        out3.low();
+
+                    } else if (ileriGeri == 1) {
+                        out0.low();
+                        out4.low();
+                        out2.high();
+                        out3.high();
+
+                    }
+                } else if (message != null && message.equals("Sol")) {
+                    Server.Display("Sol");
+                    out0.low();
+                    out2.high();
                     out3.low();
                     out4.high();
                     Thread.sleep(500);
                     out4.low();
+                    out2.low();
 
-                } else if (message != null && message.equals("Sol")) {
-                    Server.Display("Sol");
-                    out4.low();
-                    out2.low();
-                    out3.low();
-                    out0.high();
-                    Thread.sleep(500);
-                    out0.low();
-                    
-                } else if (message != null && message.equals("360")) {
-                    out4.low();
-                    out2.low();
-                    out3.low();
-                    out0.high();
-                    Thread.sleep(1750);
-                    out0.low();
-                    
+                    if (ileriGeri == 0) {
+
+                        out0.high();
+                        out4.high();
+                        out2.low();
+                        out3.low();
+
+                    } else if (ileriGeri == 1) {
+                        out0.low();
+                        out4.low();
+                        out2.high();
+                        out3.high();
+
+                    }
+
                 } else if (message != null && message.equals("Dur")) {
-                    Server.Display("Dur");
                     out4.low();
                     out2.low();
                     out3.low();
                     out0.low();
+                    ileriGeri = 2;
+
+                } else if (message != null && message.equals("kapat")) {
+                    System.exit(0);
                 }
+
             }
 
         } catch (IOException ex) {
-            Logger.getLogger(Testserver.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Testserver.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception e) {
-            Logger.getLogger(Testserver.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 
-    public static void Display(String msg) { 
-        System.out.println(msg); 
+    public static void Display(String msg) {
+        System.out.println(msg);
     }
 
 }
